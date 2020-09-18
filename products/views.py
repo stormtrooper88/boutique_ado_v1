edee9ddddca57532c7f7d5ee1d9e3f2b0e20eb60
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-09-18 09:30:01 +0000

    Added search functionality

--- products/views.py
+++ products/views.py
@@ -8,8 +8,19 @@ def all_products(request):
 
     products = Product.objects.all()
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
